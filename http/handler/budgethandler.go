7ac5b1dc3e586c4e07ac931f9417308937eb84e5
Marian Hahne <marian@marianhahne.de>
2022-09-01 17:02:44 +0200

    Add logging middleware

--- http/handler/budgethandler.go
+++ http/handler/budgethandler.go
@@ -2,12 +2,9 @@ package handler
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func ListBudgetsHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("ListBudgetsHandler called")
-
 	fmt.Fprintf(w, "List budgets")
 }
--- main.go
+++ main.go
@@ -12,8 +12,15 @@ func main() {
 	host := "localhost"
 	port := "8080"
 
-	http.HandleFunc("/budgets", handler.ListBudgetsHandler)
+	http.HandleFunc("/budgets", logging(handler.ListBudgetsHandler))
 
 	log.Printf("Listening on %s:%s", host, port)
 	http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), nil)
+}
+
+func logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		f(w, r)
+	}
 }
