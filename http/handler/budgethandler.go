6d6836b302bb20d2a0d4fe7d70a369a3567974e1
Marian Hahne <marian@marianhahne.de>
2022-09-02 13:28:16 +0200

    Add template for budget index page

--- http/handler/budgethandler.go
+++ http/handler/budgethandler.go
@@ -2,9 +2,29 @@ package handler
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type ListBudgetStruct struct {
+	Title string
+}
+
 func ListBudgetsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "List budgets")
+	data := ListBudgetStruct{
+		Title: "Budgets",
+	}
+
+	// load template from file /template/budget/index.html
+	tmpl, err := template.ParseFiles("template/budget/index.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// render template with data
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
