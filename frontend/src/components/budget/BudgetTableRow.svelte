c89ddf2cd7ee57e8bd7511be033994978cde8b46
Marian Hahne <marian@marianhahne.de>
2022-09-08 18:47:29 +0200

    Add component for BudgetTableRow

---
+++ frontend/src/components/budget/BudgetTableRow.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let title: string;
+    export let amount: number;
+    export let frequency: string;
+    export let category: string ;
+    export let description: string;
+</script>
+
+<tr>
+    <td>{title}</td>
+    <td>{amount}</td>
+    <td>{frequency}</td>
+    <td>{category}</td>
+    <td>{description}</td>
+    <td>
+        <button class="btn btn-primary">Edit</button>
+        <button class="btn btn-danger">Delete</button>
+    </td>
+</tr>
